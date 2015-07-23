41e03dfc8e0a921ce594d0d4c7c47490bc766223
maechabin <mail@chab.in>
2015-07-23 12:38:55 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,58 +1,53 @@
-(function ($, QUnit, window, document, undefined) {
+;(function ($, QUnit, window, document, undefined) {
 
   "use strict";
 
   QUnit.module("slide_down", {
     beforeEach: function () {
+      window.scroll(0, 0);
       this.header1 = $(".header1");
+      this.headerHeight = this.header1.height();
       this.header1.cbSlideDownHeader();
-      this.header2 = $(".header2");
-      this.header2.cbSlideUpHeader();
-      window.scrollTo(0, 0);
+    },
+    afterEach: function () {
+      window.scroll(0, 0);
     }
   });
   QUnit.test("$.fn.cbSlideDownHeader()が読み込まれているか", function (assert) {
     assert.ok($.fn.cbSlideDownHeader, "Passed!!")
   });
   QUnit.test("メソッドを実行する要素のTOPのデフォルト値は、要素の高さの負の値になっているか", function (assert) {
-    var headerHeight = this.header1.height();
-    assert.equal(this.header1.css("top"), "-" + headerHeight + "px", "Passed!!");
+    assert.equal(this.header1.css("top"), "-" + this.headerHeight + "px", "Passed!!");
   });
+
   QUnit.test("1000pxスクロールした時のヘッダバーのTOPの値は0以上", function (assert) {
 
     var self = this;
+    var done = assert.async();
 
     console.log(self.header1.css("top"));
     console.log(self.header1.offset().top);
-    assert.ok(self.header1.offset().top === -56, "Passed!!");
-              window.scrollTo(100, 0);
+    assert.equal(self.header1.css("top"), "-" + self.headerHeight + "px", "Passed!!");
 
-    window.setTimeout(function () {
+    window.scroll(0, 10);
+
+    setTimeout(function () {
       console.log(self.header1.css("top"));
       console.log(self.header1.offset().top);
-      assert.ok(self.header1.offset().top === 0, "Passed!!");
-
-    }, 1000);
-window.scrollTo(0, 0);
-      console.log(self.header2.css("top"));
-      console.log(self.header2.offset().top);
-      assert.ok(self.header2.css("top") === "0px", "Passed!!");
-            window.scrollTo(100, 0);
-      window.setTimeout(function () {
-        console.log(self.header2.css("top"));
-        console.log(self.header2.offset().top);
-        assert.ok(self.header2.css("top") === "-56px", "Passed!!");
-      }, 3000);
-
-
-
+      assert.equal(self.header1.css("top"), "0px", "Passed!!");
+      done();
+    }, 500);
   });
-
 
   QUnit.module("slide_up", {
     beforeEach: function () {
+      window.scroll(0, 0);
       this.header2 = $(".header2");
+      this.headerHeight = this.header2.height();
       this.header2.cbSlideUpHeader();
+    },
+    afterEach: function () {
+      window.scroll(0, 0);
     }
   });
   QUnit.test("$.fn.cbSlideUpHeader()が読み込まれているか", function (assert) {
@@ -63,19 +58,22 @@ window.scrollTo(0, 0);
   });
 
   QUnit.test("1000pxスクロールした時のヘッダバーのTOPの値は0以下", function (assert) {
-/*
+
     var self = this;
-    console.log(self.header2.offset().top);
-    assert.ok(self.header2.offset().top === 0, "Passed!!");
+    var done = assert.async();
+
+    console.log(self.header2.css("top"));
+    console.log(self.header2.offset());
+    assert.equal(self.header2.css("top"), "0px", "Passed!!");
+
     window.scroll(0, 1000);
 
     setTimeout(function () {
-      console.log(self.header2.offset().top);
-      assert.ok(self.header2.offset().top === -56, "Passed!!");
-    }, 1000);
-*/
-
-  });
-
+    console.log(self.header2.css("top"));
+    console.log(self.header2.offset());
+    assert.equal(self.header2.css("top"), "-" + self.headerHeight + "px", "Passed!!");
+    done();
+    }, 500);
+});
 
 } (jQuery, QUnit, window, document));
