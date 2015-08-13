a3da715864cdf45928de1c941645a73d74c66d26
maechabin <mail@chab.in>
2015-08-14 00:20:59 +0900

    v0.3.2

--- dist/cbslideheader.css
+++ dist/cbslideheader.css
@@ -1,6 +1,6 @@
 /**
  * jquery.cb-slideheader.js - A jQuery plugin to display or hide headerbar with a sliding motion
- * @version v0.3.1
+ * @version v0.3.2
  * @author maechabin <mail@chab.in> http://mae.chab.in/
  * @license MIT license
  */
